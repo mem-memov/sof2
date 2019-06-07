ad0e48dd3379c53949015492592123829c1fedeb
Alexander Novikov <mem-memov@yandex.ru>
2019-06-08 01:31:33 +0300

    extracting query param

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -7,6 +7,6 @@ import play.api.mvc._
 @Singleton
 class SearchController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def index(tags: List[String]) = Action { implicit request: Request[AnyContent] =>
-    Ok("111" + tags.toString())
+    Ok("tags: " + tags.toString())
   }
 }
